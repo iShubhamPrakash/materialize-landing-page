6dd78b82483b4bbef953212c41867aecd9e0b2a6
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-10-22 11:57:35 +0530

    Add go to top button

--- scripts/script.js
+++ scripts/script.js
@@ -15,4 +15,19 @@ $(document).ready(function () {
     //for tabs
     // $('.tabs').tabs();
 
+
+    // For go to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 });
