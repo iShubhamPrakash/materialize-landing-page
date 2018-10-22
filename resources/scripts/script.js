667615f0b6c6120f549cc6d1ee5727dfb3b402eb
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-10-22 10:01:14 +0530

    Add navbar

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -4,3 +4,15 @@ SHUBHAM PRAKASH
 LinkedIn: https://www.linkedin.com/in/ishubhamprakash/
 GitHub: https://github.com/i-shubhamprakash
 */
+
+$(document).ready(function () {
+    // initialising matetialize navbar
+    $('.sidenav').sidenav();
+
+    // Initialising materialize parallax
+    // $('.parallax').parallax();
+
+    //for tabs
+    // $('.tabs').tabs();
+
+});
